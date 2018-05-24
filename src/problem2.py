d4d69ed988413bc040aced04abd684a5493ca382
Todd Kuebelbeck <kuebeltm@rose-hulman.edu>
2018-05-24 13:32:40 -0400

    Committing and pushing 2a - Todd Kuebelbeck

--- src/problem2.py
+++ src/problem2.py
@@ -2,9 +2,9 @@
 Final exam, problem 2.
 
 Authors: David Mutchler, Dave Fisher, Matt Boutell, Amanda Stouder,
-         their colleagues and PUT_YOUR_NAME_HERE.  May 2018.
+         their colleagues and Todd Kuebelbeck.  May 2018.
 
-"""  # TODO: 1. PUT YOUR NAME IN THE ABOVE LINE.
+"""  # DONE: 1. PUT YOUR NAME IN THE ABOVE LINE.
 
 import time
 import testing_helper
@@ -19,7 +19,7 @@ def main():
     print()
     print('Un-comment the calls in MAIN one by one')
     print(' to run the testing code as you complete the TODOs.')
-    # run_test_problem2a()
+    run_test_problem2a()
     # run_test_problem2b()
     # run_test_problem2c()
 
@@ -170,9 +170,22 @@ def problem2a(x, sequence):
       :type sequence:  [int]
     """
     # -------------------------------------------------------------------------
-    # TODO: 2. Implement and test this function.
+    # DONE: 2. Implement and test this function.
     #          Tests have been written for you (above).
     # -------------------------------------------------------------------------
+    """""
+    Approach:
+        - Create a list
+        - Use a for loop to traverse the list from 0 to len(numbers)-1.
+        -In for loop, use if statement to check if sequence[k] is > x
+        - Return list
+     """""
+    my_list = list()
+
+    for k in range(0, len(sequence)-1):
+        if sequence[k] > x:
+            my_list.append(sequence[k])
+    return my_list
 
 
 def run_test_problem2b():
